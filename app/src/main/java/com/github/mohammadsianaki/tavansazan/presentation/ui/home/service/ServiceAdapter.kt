257189953e88b86e166d76f4f9bab0fcd518ef08
Mohammad Sianaki <m.rsianaki@yahoo.com>
2020-11-29 23:17:01 +0330

    Fix: show newBadge in item_service

--- app/src/main/java/com/github/mohammadsianaki/tavansazan/presentation/ui/home/service/ServiceAdapter.kt
+++ app/src/main/java/com/github/mohammadsianaki/tavansazan/presentation/ui/home/service/ServiceAdapter.kt
@@ -3,6 +3,8 @@ package com.github.mohammadsianaki.tavansazan.presentation.ui.home.service
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
+import com.github.mohammadsianaki.core.extensions.gone
+import com.github.mohammadsianaki.core.extensions.visible
 import com.github.mohammadsianaki.core.ui.adapter.BaseRecyclerAdapter
 import com.github.mohammadsianaki.core.ui.adapter.BaseViewHolder
 import com.github.mohammadsianaki.tavansazan.databinding.ItemServiceBinding
@@ -24,6 +26,7 @@ class ServiceViewHolder(
     override fun bindData(item: ServiceCategoryItemModel) {
         binding.root.setOnClickListener { onItemClickListener(item, adapterPosition) }
         with(binding) {
+            if (true) badge.visible() else badge.gone()
             Glide.with(serviceLogo.context).load(item.imageUrl).into(serviceLogo)
             serviceTitle.text = item.title
             serviceSubTitle.text = item.subTitle
